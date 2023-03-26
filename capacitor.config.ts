28fe5c0fcd5d523a857e565e363f41d06e2b7a65
MatiasO <matiaz.orellana@gmail.com>
2023-03-26 12:51:21 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ws-client2-example',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
